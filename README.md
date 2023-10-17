4b8013f46f240e96f3dfe6ccb09823bd467ea9c4
Kassia Zheng <nz2280@nyu.edu>
2023-10-17 12:53:44 -0700

    1

--- README.md
+++ README.md
@@ -1,60 +1,34 @@
-# p5.js Template
+# Midterm Project
 
-This is a README file that can be used to describe and document your assignment.
+# Describtion
+"Isle of Dogs" is a stop-motion animated film directed by Wes Anderson. It was released in 2018 and is known for its unique visual style and storytelling. The film is set in a dystopian near-future Japan and follows the story of a young boy's quest to find his lost dog.
 
-Markdown Cheatsheet (from [https://www.markdownguide.org/cheat-sheet/](https://www.markdownguide.org/cheat-sheet/)):
+The movie was set in an outbreak of the canine flu that has led to the banishment of all dogs to Trash Island, a remote, polluted wasteland. The authoritarian Mayor Kobayashi is responsible for this decree, and he exiles all dogs to ensure the safety of the human population. Among the exiled dogs is Spots, the beloved pet of a young boy named Atari. And he decided to go to Trash Island to find his dog. Along the way, he encounters a group of other exiled dogs who help him in his search.
 
----
----
+The film explores loyalty, friendship, and the bond between humans and dogs. It also delves into political corruption, discrimination, and the importance of standing up for what is right. As Atari and the dogs work together to find Spots, they uncover a conspiracy that goes beyond the banishment of dogs and set out to challenge the status quo.
 
-# Heading1
-## Heading2
-### Heading3
-#### Heading4
-##### Heading5
-###### Heading6
+## My idea
+The story centers around a young boy embarking on a heartfelt quest to reunite with his cherished dog. To create an engaging, interactive cover, I envisioned a poignant scene where a faithful dog eagerly waits amid a desolate garbage dump. I incorporated an alarm clock into the illustration to emphasize the passage of time during this waiting.
 
-**bold text**
+Furthermore, I aimed to capture the essence of the narrative by highlighting the transformative power of the boy's touch. When the mouse pointer makes contact with the dog on the cover, the dog's expression shifts from one of solitude to one filled with pure happiness, as if the long-awaited reunion with his owner has finally come to fruition.
 
-*italicized text*
+The themes and elements from the movie align with the readings we've explored. For instance, the film portrays a society where morally compromised officials conspire to replace real dogs with robotic pets for financial gain. This mirrors the profit-driven motives we discussed in our readings. Although the dogs are exiled to junkyards, they demonstrate resilience by forging their identities and embracing life, reflecting how people navigate the complexities of the digital world and the internet.
 
-~~strikethrough text~~
+### My work be like
+I will create an image depicting the young boy tumbling from his plane, his flying goggles revealing a captivating sight. In front of him, five dogs come into view, and the one at the center is the very dog he's been seeking. When a treat is playfully waved from side to side, the dog's eyes irresistibly follow the motion of the hand. And with a simple click on the black dog, his fur magically transforms from ebony to pristine white, just as if the boy has successfully reunited with his long-lost canine companion.
 
-Ordered List:
-1. First item
-2. Second item
-3. Third item
+This is my ideal collage
+![D0b8Dg.jpg](https://imgpile.com/images/D0b8Dg.jpg)
+This is my plan with how to draw in coding 
+![D0d4S4.jpg](https://imgpile.com/images/D0d4S4.jpg)
 
-Unordered List:
-- First item
-- Second item
-- Third item
-
-`short code block`
-
-```
-extended code block
-fun() {
-  return 0
-}
-```
-
-Link:  
-[linked text](https://www.example.com)
+I had diffuclty doing the car in the back
+I try to use for loops we learned before, but there are some issues
+![DQzOxX.jpg](https://imgpile.com/images/DQzOxX.jpg)
+I also searched online a new quick way, it works with others but not the car, I guess its beacuse I have too much layer.
+// push();
+  // line(1, 99, 1500, 99); //linebehind
+  // drawingContext.setLineDash([5, 15]);
+  // pop();
 
 
-Image with url:  
-![image description](https://dm-gy-6063-2023f-d.github.io/assets/homework/02/clark-espaco-modulado-00.jpg)
-
-
-Image on repo:  
-![image description](./file-name.jpg)
-
-
-To start a new line, add two spaces at the end of a line, like this:  
-this is a new line.
-
-
-To start a new paragraph, leave an empty line between two lines of text.
-
-This is a new paragraph.
--- sketch.js
+++ sketch.js
@@ -1,6 +1,300 @@
-function setup() {
+
+// function drawcookie(x, y, diameter){
+//   stroke('black');
+//   strokeWeight(3);
+//   fill(232, 103, 177);
+//   ellipse(x-diameter/2, y, diameter);
+//   ellipse(x+diameter/2, y, diameter);
+//   fill('yellow');
+//   ellipse(x, y, diameter/2, diameter/2);
+
+// }
+
+
+  function setup() {
   createCanvas(windowWidth, windowHeight);
+  angleMode(DEGREES);
 }
 
-function draw() {
+function drawglasses() {
+  fill("black");
+  rect(1, 1, 2000, 900);
+  fill("white");
+  rect(70, 50, 1300, 600, 80);
+  fill("black");
 }
+
+function drawtheveryleftdog() {
+  fill(105, 105, 96);
+  rect(190, 450, 30, 205); //leg
+  fill(168,168, 162);
+  rect(150, 450, 30, 205);//leg
+  rect(50, 260, 195, 195, 20);//body
+
+  //ear
+  fill(97, 97, 91);
+  triangle(136, 113, 165, 44, 186, 100);
+  triangle(201, 100, 228, 49, 247, 122);
+  //face
+  fill('grey');
+  ellipse(186, 190, 190, 180);
+  //eye
+  fill("white");
+  ellipse(150, 170, 25, 25); //left
+  ellipse(220, 170, 25, 25); //right
+  //eyeball
+  fill("black");
+  ellipse(150, 170, 10, 10); //left
+  ellipse(220, 170, 10, 10); //right
+  fill(178, 178, 171);
+  triangle(160, 199, 216, 199, 189, 235); //nose
+  fill("black");
+  ellipse(198, 210, 7, 5); //noseholeleft
+  ellipse(178, 210, 7, 5); //noseholeright
+  line(189, 199, 189, 235);
+  //mouse
+  line(162, 247, 200, 247);
+}
+
+function drawcar() {
+  circle(320, 100, 23);
+  circle(370, 100, 23);
+  square(308, 145, 75);
+  line(345, 100, 345, 145);
+  // push();
+  // line(1, 99, 1500, 99); //linebehind
+  // drawingContext.setLineDash([5, 15]);
+  // pop();
+  fill('black');
+  line(300, 99, 240, 99);
+
+  let spacing = 200;
+  for (x = 0; x < 3000; x += spacing);
+
+  line(x, 99, x+200, 99);
+
+}
+
+function drawmiddledog() {
+  strokeWeight('3');
+  stroke("black");
+  fill(28, 12, 2);
+  ellipse(width / 2, 360, 140, 200); //body
+
+  //ear
+  fill(71, 44, 28);
+  triangle(656, 139, 673, 55, 714, 105);
+  triangle(731, 105, 751, 53, 776, 131);
+  fill(48, 44, 42);
+  strokeWeight('3');
+  ellipse(width / 2, 210, 170, 210); //middle dog head
+  rect(669, 214, 100, 100, 40); //mouse
+  line(710, 286, 733, 286); // mouseline
+  strokeWeight(3);
+  fill(112, 87, 73);
+  ellipse(width / 2, 255, 40, 40); //nouse
+  fill("black");
+  ellipse(width / 2.018, 254, 6, 6); //nouseholeleft
+  ellipse(width / 1.983, 254, 6, 6); //nouseholeright
+  line(720, 244, 720, 264);
+  fill("white");
+  ellipse(width / 2.1, 180, 25, 25); //eyeoutsideleft
+  ellipse(width / 1.94, 180, 25, 25); //eyeoutsideright
+  fill("black");
+  ellipse(width / 2.1, 180, 12, 12); //eyeinsideleft
+  ellipse(width / 1.94, 180, 12, 12); //eyeinsideright
+
+  rect(730, 445, 11, 110); //leg
+  rect(758, 427, 16, 150); //leg
+  rect(700, 445, 11, 110); //leg
+  rect(670, 427, 16, 150);
+}
+
+function drawforthdog(){
+   //ear
+   fill(230, 122, 13);
+   triangle(920, 188, 939, 119, 974, 187);
+   triangle(995, 186, 1039, 139, 1038, 199);
+   rect(942, 333, 103, 120);//body
+   rect(988, 453, 18, 90); //leg
+   rect(942, 453, 18, 125); //leg
+   rect(1031, 453, 14, 90);//leg
+   rect(999, 453, 18, 125);//leg
+
+   //mouseshape
+  push();
+  fill(235, 105, 30);
+  translate(920, 180);
+  rotate (0.04);
+  rect(0, 0, 120, 155);//head
+  pop();
+  fill(156, 68, 17);
+  rect(920, 280, 100, 70);//mouse
+
+  
+  strokeWeight('2');
+  fill("white");
+  circle(955, 216, 24); //left eye
+  fill("black");
+  circle(955, 216, 10);
+
+  strokeWeight('2');
+  fill("white"); //right eye
+  circle(1005, 216, 34);
+  fill("black");
+  circle(1005, 216, 12);
+
+  fill(201, 83, 16);
+  triangle(939, 304, 984, 302, 962, 336);
+  line(960, 304, 961, 335); // noseline
+  //nosehole
+  fill("black");
+  circle(953, 312, 7);
+  circle(969, 312, 7);
+
+}
+
+function draw2dog(){
+  //ear
+  fill(202, 202, 92);
+  triangle(312, 266, 303, 302, 331, 302);
+  triangle(351, 274, 342, 302, 368, 302);
+  rect(260, 370, 103, 100);//body
+  rect(260, 470, 12, 70); //leg
+  rect(290, 470, 14, 90); //leg
+  rect(325, 470, 12, 70);//leg
+  rect(349, 470, 14, 83);//leg
+
+ fill(230, 230, 14);
+ rect(300, 302, 69, 80);//head
+ rect(295, 340, 79, 59, 39);//mouse
+ strokeWeight('2');
+ 
+ fill('white');
+ circle(316, 327, 18); //left eye
+ fill("black");
+ circle(316, 327, 11);
+ fill("white"); //right eye
+ circle(352, 327, 18);
+ fill("black");
+ circle(352, 327, 11);
+
+ fill('white');
+ triangle(315, 362, 348, 362, 332, 387);
+ line(332, 363, 332, 387); // noseline
+ //nosehole
+ fill("black");
+ circle(327, 370, 5);
+ circle(337, 370, 5);
+
+}
+
+function drawtheveryrightdog() {
+  strokeWeight('3');
+  stroke("black");
+  fill(238, 196, 30);
+  rect(1096, 320, 23, 38, 20, 150, 10, 50); //body
+  rect(1096, 120, 230, 260); //head
+  circle(1200, 330, 230); //facemouse
+
+  fill(65, 33, 5);
+  circle(1150, 190, 57); //left eye
+  fill("white");
+  circle(1150, 190, 47);
+  fill("black");
+  circle(1150, 190, 22);
+
+  fill(65, 33, 5); //right eye
+  circle(1270, 190, 57);
+  fill("white");
+  circle(1270, 190, 47);
+  fill("black");
+  circle(1270, 190, 22);
+
+  //ear
+  fill(154, 125, 12);
+  triangle(1098, 120, 1138, 23, 1185, 120);
+  triangle(1253, 120, 1275, 23, 1324, 120);
+
+  //mouseshape
+  fill(65, 33, 5);
+  triangle(1150, 295, 1248, 295, 1200, 384);
+  line(1240, 420, 1200, 420); // mouseline
+  //nosehole
+  fill("black");
+  circle(1183, 326, 13);
+  circle(1218, 326, 13);
+  line(1200, 310, 1200, 360); //noseline
+ 
+}
+
+function drawclock(){
+  strokeWeight(2);  
+  fill(68, 92, 2); //body cicle clock 
+  ellipse(346, 183, 65, 65);
+  
+  strokeWeight(2);
+  fill('pink'); //inside cicle clock
+  ellipse(346, 183, 10, 10);
+
+    // Calculate the current time
+    let hr = hour();
+    let min = minute();
+    let sec = second();
+  
+    push();
+  translate(347, 183); // Move the origin to the center
+  noFill();
+  stroke('black');
+  strokeWeight(4);
+
+    // Hour hand
+    strokeWeight(4);
+    stroke(60,135,252);
+    let hrAngle = map(hr % 12, 0, 12, 0, 360) + map(min, 0, 60, 0, 30); // Calculate hour hand angle
+    rotate(hrAngle);
+    line(0, 0, 24, 0);
+    rotate(-hrAngle);
+   
+  
+    // Minute hand
+    strokeWeight(2);
+    let minAngle = map(min, 0, 60, 0, 360); // Calculate minute hand angle
+    rotate(minAngle);
+    line(0, 0, 30, 0);
+    rotate(-minAngle);
+
+     // Second hand
+  stroke('black');
+  stroke(255, 0, 0); // Red color for the second hand
+  strokeWeight(2);
+  let secAngle = map(sec, 0, 60, 0, 360); // Calculate second hand angle
+  rotate(secAngle);
+  line(0, 0, 28, 0);
+}
+
+
+function draw() {
+  drawglasses();
+  drawcar();
+  drawmiddledog();
+  drawforthdog();
+  drawtheveryrightdog();
+  drawtheveryleftdog();
+  draw2dog();
+  drawclock();
+
+  stroke("black");
+  fill("white");
+
+  //cookie
+drawcookie(mouseX,mouseX, mouseY, mouseY);
+
+
+}
+
+function mousePressed() {
+  console.log(mouseX, mouseY);
+}
+
+
